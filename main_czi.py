20fa31b7b44c52a9c6244adc69afa273df85c31e
Kasra Arnavaz <kasrar@theia.imm.dtu.dk>
2021-11-18 15:02:26 +0100

    11/18/2021

--- main_czi.py
+++ main_czi.py
@@ -6,10 +6,9 @@ import shutil
 
 def main():
 
-    czi_path = '../raw_data/new_czi'
+    czi_path = '../raw_data/new_czi/sus'
     names = [name.replace('.czi', '') for name in os.listdir(czi_path) if name.endswith('.czi')]
-    for name in reversed(names):
-        print(name)
+    for name in names:
         tif_path = f'../results/new_czi/{name}/tif'
         if not os.path.exists(tif_path):
            czi2tif(czi_path, name, tif_path)
@@ -17,6 +16,7 @@ def main():
            semi = SemiSupervised(name='semi', resume_epoch=40, final_epoch=40, loss_weights=[1, 10], transformer=ModifiedStandardization)
            semi.test_model(tif_path, write_path = f'{tif_path}/..')
         shutil.rmtree(tif_path)
+        
 
 if __name__ == '__main__':
     main()
--- utils/cyc_tif.py
+++ utils/cyc_tif.py
@@ -11,15 +11,15 @@ from time_filtering.trackpy_filtering import TrackpyFiltering
 
 class Topo2Tif:
 
-    def __init__(self, cyc_path, pred_file, cyctpy_path=None):
-        if cyctpy_path is None: cyctpy_path = cyc_path
+    def __init__(self, cyc_path, cyc_name, pred_file):
         self.cyc_path = cyc_path
-        self.cyctpy_path = cyctpy_path
         self.pred_file = pred_file
-        self.names = [name.replace('.cyctpy', '') for name in os.listdir(self.cyctpy_path) if name.endswith('.cyctpy')]
+        self.num_frames = len([name for name in os.listdir(self.cyc_path) if name.endswith('.cyc')])
+        #self.num_frames = 3
+        self.names = [f'{cyc_name}_tp{i+1}' for i in range(self.num_frames)]
 
-    def topology(self, path, name, extension):
-        return read_pickle(f'{path}/{name}.{extension}')
+    def topology(self, name, extension):
+        return read_pickle(f'{self.cyc_path}/{name}.{extension}')
     
     @property
     def shape(self):
@@ -29,9 +29,8 @@ class Topo2Tif:
         list_node_pos = [v for v_list in topology.position.values() for v in v_list]
         pos_dict = {}
         for node, pos in zip(topology.node_list, list_node_pos):
-            pos[0] /= 2.89
-            pos = np.round(pos).astype(int)
-            pos_dict[node] = pos
+            position = pos / (np.array([2.89, 1, 1]))
+            pos_dict[node] = np.round(position).astype(int)
         return pos_dict
 
     @staticmethod
@@ -51,7 +50,6 @@ class Topo2Tif:
         return tuple(z_list), tuple(y_list), tuple(x_list)
     
     def write_tif(self):
-        num_frames = len(self.names)
         # with tif.TiffWriter('temp.tif') as tiff:
         #     for tp in range(5):
         #         binary = np.zeros(self.shape + (3,), dtype='uint8') # 3 for rgb channels
@@ -59,11 +57,11 @@ class Topo2Tif:
         #         for edges, loop_id in zip(topology.topology_edges, topology.loop_id):
         #             binary[self.skel_indices(edges, topology)] = colors[loop_id%len(colors)]
         #         tiff.save(binary, contiguous=True)
-        binary = np.zeros((num_frames,)+self.shape+(3,), dtype='uint8')
+        binary = np.zeros((self.num_frames,)+self.shape+(3,), dtype='uint8')
         for tp, name in enumerate(self.names):
             print(name)
-            cyc = self.topology(self.cyc_path, name, 'cyc')
-            cyctpy = self.topology(self.cyctpy_path, name, 'cyctpy')
+            cyc = self.topology(name, 'cyc')
+            cyctpy = self.topology(name, 'cyctpy')
             i = 0
             for cyc_edges in cyc.topology_edges:
                 if not cyc_edges in cyctpy.topology_edges: #filtered
@@ -88,7 +86,7 @@ class Topo2Tif:
 
 
 if __name__ == '__main__':
-    t2t = Topo2Tif('mock_movie', 'movie/test/LI_2018-11-20_emb7_pos4/pred/pred-0.7-semi-40_2018-11-20_emb7_pos4_tp38.tif')
+    t2t = Topo2Tif('mock_movie', 'pred-0.7-semi-40_2018-11-20_emb7_pos4', '../results/test-old/LI_2018-11-20_emb7_pos4/pred/pred-0.7-semi-40_2018-11-20_emb7_pos4_tp38.tif')
     t2t.write_tif()
 
     # t2t.show_mip()

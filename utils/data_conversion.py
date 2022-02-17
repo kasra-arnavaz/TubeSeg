5426090b6c2d61c3016aa6e3f3b885efbf0fbd2a
Kasra Arnavaz <kasra@di.ku.dk>
2022-02-17 13:06:05 +0100

    new naming system

--- utils/data_conversion.py
+++ utils/data_conversion.py
@@ -1,77 +1,82 @@
-from utils.data_utils import *
-from abc import ABC, abstractmethod
+import tifffile as tif
+import numpy as np
+import os
+from abc import ABC, abstractmethod, abstractproperty
 
 class Conversion(ABC):
 
-    def __init__(self, name_parser: NameParser, input_pic: np.ndarray):
-        self.name_parser = name_parser
-        self.input_pic = input_pic
+    def __init__(self, input_name: str, input_img: np.ndarray, write_path:str, *args):
+        self.input_name = input_name
+        self.input_img = input_img
+        self.write_path = write_path
+        self.args = args
     
     @abstractmethod
-    def convert(self, *args):
+    def convert(self):
         pass
+
+    @abstractproperty
+    def output_name(self):
+        pass
+
+    def write(self):
+        os.makedirs(self.write_path, exist_ok=True)
+        tif.imwrite(f'{self.write_path}/{self.output_name}', self.convert())
+
+
 
 
 class Duct2Mip(Conversion):
+    # maximum intensity projection
+    def convert(self):
+        return np.amax(self.input_img, axis=0)
 
-    def convert(self, *args):
-        mip = np.amax(self.input_pic, axis=0)   # maximum intensity projection
-        self.name_parser.category = 'duct-mip'
-        return self.name_parser, mip
+    @property
+    def output_name(self):
+        return self.input_name.replace('duct', 'duct-mip')
 
 
 class Prob2Pred(Conversion):
 
-    def convert(self, *args):
-        threshold = args[0]
-        pred = ((self.input_pic >= threshold)*255).astype('uint8')
-        self.name_parser.category = self.name_parser.category.replace('prob', f'pred-{threshold}')
-        return self.name_parser, pred
+    def convert(self):
+        return ((self.input_img >= self.args[0])*255).astype('uint8')
+    
+    @property
+    def output_name(self):
+        return self.input_name.replace('prob', f'pred-{self.args[0]}')
 
 
 class Prob2Ent(Conversion):
 
-    def convert(self, *args):
+    def convert(self):
         from scipy.stats import entropy
-        p = np.stack((self.input_pic, 1-self.input_pic), axis=0)
-        ent = entropy(p, base=2, axis=0)
-        self.name_parser.category = self.name_parser.category.replace('prob', 'ent')
-        return self.name_parser, ent
+        p = np.stack((self.input_img, 1-self.input_img), axis=0)
+        return entropy(p, base=2, axis=0)
+    
+    @property
+    def output_name(self):
+        return self.input_name.replace('prob', 'ent')
     
 
 class Lin2Prob(Conversion):
 
-    def convert(self, *args):
-        a, b = args
-        z = a*self.input_pic + b
-        prob = 1/(1+np.exp(-z))
-        self.name_parser.category = self.name_parser.category.replace('lin', 'prob')
-        return self.name_parser, prob
+    def convert(self):
+        a, b = self.args
+        z = a*self.input_img + b
+        return 1/(1+np.exp(-z))
+
+    @property
+    def output_name(self):
+        return self.input_name.replace('lin', 'prob')
 
 
-class Ent2ThrEnt(Conversion):
 
-    def convert(self, *args):
-        threshold = args[0]
-        thr_ent = ((self.input_pic >= threshold)*255).astype('uint8')
-        self.name_parser.category = self.name_parser.category.replace('ent', f'ent-{threshold}')
-        return self.name_parser, thr_ent
-
-
-class MassDataConversion:
-
-    def __init__(self, read_path, write_path, conversion_type: Conversion, *args):
-        self.read_path = read_path
-        self.conversion_type = conversion_type
-        self.write_path = write_path
-        self.args = args
-    
-    def read_convert_write(self):
-        for input_name, input_pic in TifReader(self.read_path):
-            output_name, output_pic = self.conversion_type(input_name, input_pic).convert(*self.args)
-            TifWriter(self.write_path, output_name, output_pic).write()
+def MassDataConversion(input_path:str , write_path: str, converter: Conversion, *args):
+    names = [name for name in os.listdir(input_path) if name.endswith('.tif')]
+    for name in names:
+        input_img = tif.imread(f'{input_path}/{name}')
+        converter(name, input_img, write_path, args).write()
 
 
 if __name__ == '__main__':
-    # for i in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-    MassDataConversion('D:/pia', 'D:/pia/mip', Duct2Mip).read_convert_write()
+    MassDataConversion('../alaki', '../alaki/mip', Duct2Mip)
